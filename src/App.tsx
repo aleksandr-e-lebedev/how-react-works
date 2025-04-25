ec88c4221ccf69e820ad02cc424d8cf3bda96285
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-04-25 13:51:28 +0300

    feat: build UI of TabContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,89 @@
+interface ItemType {
+  summary: string;
+  details: string;
+}
+
+interface TabContentProps {
+  item: ItemType;
+}
+
+function TabContent({ item }: TabContentProps) {
+  const showDetails = true;
+  const likes = 0;
+
+  function handleToggleDetails() {
+    return;
+  }
+
+  function handleIncrementLikes() {
+    return;
+  }
+
+  function handleTrippleIncrementLikes() {
+    return;
+  }
+
+  function handleUndo() {
+    return;
+  }
+
+  function handleUndoLater() {
+    return;
+  }
+
+  return (
+    <div className="tab-content">
+      <h4 className="tab-content__title">{item.summary}</h4>
+      {showDetails && <p className="tab-content__details">{item.details}</p>}
+
+      <div className="tab-content__actions">
+        <button
+          type="button"
+          className="tab-content__action"
+          onClick={handleToggleDetails}
+        >
+          {showDetails ? 'Hide' : 'Show'} details
+        </button>
+
+        <div className="tab-content__hearts-counter">
+          <span className="tab-content__hearts-count">{likes} ❤️</span>
+          <button
+            type="button"
+            className="tab-content__hearts-action"
+            onClick={handleIncrementLikes}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            className="tab-content__hearts-action"
+            onClick={handleTrippleIncrementLikes}
+          >
+            +++
+          </button>
+        </div>
+      </div>
+
+      <div className="tab-content__undo">
+        <button
+          type="button"
+          className="tab-content__undo-button"
+          onClick={handleUndo}
+        >
+          Undo
+        </button>
+        <button
+          type="button"
+          className="tab-content__undo-button"
+          onClick={handleUndoLater}
+        >
+          Undo in 2s
+        </button>
+      </div>
+    </div>
+  );
+}
+
 interface TabProps {
   num: number;
   activeTab: number;
