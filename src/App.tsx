3d71e135a6b003a5808298443f614da68b5fe0d6
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-04-25 13:14:43 +0300

    feat: build UI of Tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+interface TabProps {
+  num: number;
+  activeTab: number;
+  onActivateTab: (num: number) => void;
+}
+
+function Tab({ num, activeTab, onActivateTab }: TabProps) {
+  return (
+    <button
+      type="button"
+      className={activeTab === num ? 'tab tab_active' : 'tab'}
+      onClick={() => {
+        onActivateTab(num);
+      }}
+    >
+      Tab {num + 1}
+    </button>
+  );
+}
+
 export default function App() {
   return (
     <div className="app">
